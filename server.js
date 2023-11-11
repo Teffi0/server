8ba8e8be805097739f95110f0c9a22e475b61754
Teffi0 <gre_13@mail.ru>
2023-11-11 19:43:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -81,40 +81,157 @@ function sendPushNotification(deviceToken, title, body) {
 
 app.use(bodyParser.json());
 
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
+app.get('/services', (req, res) => {
+  const sql = 'SELECT service_name FROM services'; // SQL-запрос для извлечения данных из таблицы services
 
-  if (!username || !password) {
-    return res.status(400).json({ error: 'Введите логин и пароль' });
-  }
-
-  const sql = 'SELECT * FROM users WHERE username = ?';
-  db.query(sql, [username], (err, results) => {
+  db.query(sql, (err, results) => {
     if (err) {
       console.error('Ошибка при выполнении запроса к базе данных:', err.message);
       return res.status(500).json({ error: 'Внутренняя ошибка сервера' });
     }
 
-    if (results.length === 0) {
-      return res.status(401).json({ error: 'Пользователь не найден' });
-    }
-
-    const user = results[0];
-
-    bcrypt.compare(password, user.password, (compareErr, isMatch) => {
-      if (compareErr) {
-        console.error('Ошибка при сравнении паролей:', compareErr.message);
-        return res.status(500).json({ error: 'Внутренняя ошибка сервера' });
-      }
-
-      if (!isMatch) {
-        return res.status(401).json({ error: 'Неправильный пароль' });
-      }
-
-      res.status(200).json({ message: 'Вход выполнен успешно', user: { id: user.id, username: user.username } });
-    });
+    const serviceNames = results.map(result => result.service_name);
+    res.status(200).json(serviceNames);
   });
 });
+
+app.get('/paymentmethods', (req, res) => {
+  const sql = 'SELECT payment FROM paymentmethod'; // SQL-запрос для извлечения данных из таблицы paymentmethod
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error('Ошибка при выполнении запроса к базе данных:', err.message);
+      return res.status(500).json({ error: 'Внутренняя ошибка сервера' });
+    }
+
+    const paymentMethods = results.map(result => result.payment);
+    res.status(200).json(paymentMethods);
+  });
+});
+
+app.get('/employees', (req, res) => {
+  const sql = 'SELECT full_name FROM employees'; // SQL-запрос для извлечения данных из таблицы employees
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error('Ошибка при выполнении запроса к базе данных:', err.message);
+      return res.status(500).json({ error: 'Внутренняя ошибка сервера' });
+    }
+
+    const employeeNames = results.map(result => result.full_name);
+    res.status(200).json(employeeNames);
+  });
+});
+
+app.get('/clients', (req, res) => {
+  const sql = 'SELECT full_name, address, phone_number FROM clients'; // SQL-запрос для извлечения данных из таблицы clients
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error('Ошибка при выполнении запроса к базе данных:', err.message);
+      return res.status(500).json({ error: 'Внутренняя ошибка сервера' });
+    }
+
+    const clientData = results.map(result => ({
+      full_name: result.full_name,
+      address: result.address,
+      phone_number: result.phone_number
+    }));
+    res.status(200).json(clientData);
+  });
+});
+
+app.get('/tasks', (req, res) => {
+  let sql = 'SELECT * FROM tasks';
+  const params = [];
+
+  // Если в запросе указан параметр date, фильтруем задачи по этой дате
+  if (req.query.date) {
+    sql += ' WHERE DATE(start_date) = ?';
+    params.push(req.query.date);
+  }
+
+  db.query(sql, params, (err, results) => {
+    if (err) {
+      console.error('Ошибка при выполнении запроса к базе данных:', err.message);
+      return res.status(500).json({ error: 'Внутренняя ошибка сервера' });
+    }
+
+    res.status(200).json(results);
+  });
+});
+
+app.get('/task-dates', (req, res) => {
+  const sql = 'SELECT DISTINCT DATE(start_date) AS task_date FROM tasks';
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error('Ошибка при выполнении запроса к базе данных:', err.message);
+      return res.status(500).json({ error: 'Внутренняя ошибка сервера' });
+    }
+
+    // Форматируем результат в массив дат
+    const taskDates = results.map(result => result.task_date);
+    res.status(200).json(taskDates);
+  });
+});
+
+
+app.post('/tasks', (req, res) => {
+  const { status, service, payment, cost, start_date, end_date, start_time, end_time, responsible, employees, fullname_client, address_client, phone, description } = req.body;
+
+  console.log('Поля запроса:', {
+    status,
+    service,
+    payment,
+    cost,
+    start_date,
+    end_date,
+    start_time,
+    end_time,
+    responsible,
+    employees,
+    fullname_client,
+    address_client,
+    phone,
+    description
+  });
+
+  const emptyFields = [];
+
+  if (!status) emptyFields.push('status');
+  if (!service) emptyFields.push('Услуга');
+  if (!payment) emptyFields.push('Способ оплаты');
+  if (!cost) emptyFields.push('Стоимость услуги');
+  if (!start_date) emptyFields.push('Дата начала');
+  if (!end_date) emptyFields.push('Дата окончания');
+  if (!start_time) emptyFields.push('Дедлайн');
+  if (!end_time) emptyFields.push('Время');
+  if (!responsible) emptyFields.push('Ответственный');
+  if (!employees) emptyFields.push('Участники');
+  if (!fullname_client) emptyFields.push('ФИО клиента');
+  if (!address_client) emptyFields.push('Адрес клиента');
+  if (!phone) emptyFields.push('Номер телефона клиента');
+  if (!description) emptyFields.push('Описание');
+  
+  if (emptyFields.length > 0) {
+    console.log('Ошибка 400: Отсутствующие или некорректные поля:', emptyFields);
+    return res.status(400).json({ error: 'Не все поля задачи заполнены', missingFields: emptyFields });
+  }
+  const sql = 'INSERT INTO tasks (status, service, payment, cost, start_date, end_date, start_time, end_time, responsible, employees, fullname_client, address_client, phone, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+  db.query(sql, [status, service, payment, cost, start_date, end_date, start_time, end_time, responsible, employees, fullname_client, address_client, phone, description], (err, result) => {
+    if (err) {
+      console.error('Ошибка при добавлении задачи в базу данных:', err.message);
+      return res.status(500).json({ error: 'Ошибка при добавлении задачи' });
+    }
+
+    console.log('Задача успешно добавлена:', result);
+    res.status(201).json({ message: 'Задача успешно добавлена', task_id: result.insertId });
+  });
+});
+
+
+
 
 
 // Маршрут для добавления нового клиента
